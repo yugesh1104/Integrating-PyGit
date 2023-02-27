40e22b9c5f3afcd5e9b7b32cdaa67267d9cc0396
yugesh <yugesh@kyrostechnologies.com>
2023-02-27 15:06:20 +0530

    add file name

--- integration.py
+++ integration.py
@@ -3,3 +3,6 @@ from github import Github
 g = Github("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")
 for repo in g.get_user().get_repos():
     print(repo.name)
+    contents = repo.get_contents("")
+    for file in contents:
+          print("file : ",file.name)
