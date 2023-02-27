d4d126f8b44eeb482b3f57a2e6780d4c012034d0
yugesh <yugesh@kyrostechnologies.com>
2023-02-27 14:51:32 +0530

    add

---
+++ integration.py
@@ -0,0 +1,5 @@
+from github import Github
+# Replace <access_token> with your personal access token
+g = Github("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")
+for repo in g.get_user().get_repos():
+    print(repo.name)
