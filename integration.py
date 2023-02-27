afbd5b42d8aac2705f7589afa64332db3a2ebc5f
yugesh <yugesh@kyrostechnologies.com>
2023-02-27 18:46:06 +0530

    file access

--- integration.py
+++ integration.py
@@ -1,8 +1,12 @@
 from github import Github
 # Replace <access_token> with your personal access token
-g = Github("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")
+g = Github("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")
 for repo in g.get_user().get_repos():
     print(repo.name)
     contents = repo.get_contents("")
     for file in contents:
           print("file : ",file.name)
+          
+file = repo.get_contents("integration.py")
+file_contents = file.decoded_content.decode("utf-8")
+print(file_contents)
